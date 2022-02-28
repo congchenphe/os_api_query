03eb253fefb3b1a4b0e49a1e03acb97039905314
Cong Chen <cong.chen@phe.gov.uk>
2022-02-28 16:12:15 +0000

    additional function where validation of addresses will happen separately

--- os_api_query.R
+++ os_api_query.R
@@ -18,6 +18,59 @@ OS_PLACES_UPRN_PATH <- "https://api.os.uk/search/places/v1/uprn"
 #                               TRUE - throws STOP up to calling program.
 # Returns: string containing top address match.
 
+get_address_raw <-
+    get_address <- function(building,
+                            postcode,
+                            api_key,
+                            failed_response="",
+                            stop_on_error=FALSE,
+                            rate_limit_delay = 1) {
+        tryCatch({
+            Sys.sleep(rate_limit_delay)
+
+            # build search and request strings & execute
+            building = casefold(building, upper = TRUE)
+            postcode = gsub(" ", "", casefold(postcode, upper = TRUE))
+            search_string <- paste(building, postcode)
+
+            request <- GET(url = OS_PLACES_FIND_PATH,
+                           query = list(query = search_string,
+                                        maxresults = 1,
+                                        key = api_key
+                           )
+            )
+
+            # STOP if server response status is not 200.
+            if(request$status_code != 200)
+                stop(paste("Expected 200 Status response from API, received:",
+                           request$status), call. = FALSE)
+
+            response <- content(request, as = "text", encoding = "UTF-8")
+
+            # Build a data frame containing the response
+            req <- fromJSON(response)
+
+            if(req$header$totalresults == 0)
+                return(failed_response)
+
+            df <- req$results$DPA
+            df <- add_missing_columns(df)
+
+            df <- select(df, ADDRESS, BUILDING_NUMBER, BUILDING_NAME,
+                         SUB_BUILDING_NAME, POSTCODE)
+
+            return(df$ADDRESS)
+
+        }, error = function(e) {
+            if(stop_on_error){
+                stop(e)  # Throw error to calling program.
+            } else {
+                return(paste(e))  # return caught error as a string.
+            }
+        })
+    }
+
+
 get_address <- function(building,
                         postcode,
                         api_key,
